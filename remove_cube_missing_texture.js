0c96219ddeedcac5e2c3e82ab71c701f2661df42
phisut kosayamat <dady_ocat@hotmail.com>
2025-01-09 18:50:11 +0700

    fix

--- remove_cube_missing_texture.js
+++ remove_cube_missing_texture.js
@@ -1,50 +1,48 @@
-(function() {
-var flash_action;
-Plugin.register('remove_cube_missing_texture', {
-	title: 'Remove Cube Missing Texture',
-	icon: 'flash_on',
-	author: 'kigkosa',
-	description: 'Remove Cube Missing Texture',
-	version: '1.1.1',
-	variant: 'both',
-	onload() {
-		const mt_highlighter = {
-			i: 0,
-			start: function() {
-				mt_highlighter.i = 0;
-				mt_highlighter.flash();
-			},
-			flash: function() {
-				
-				Cube.all.forEach((m, i) => {
-					
-					let del = false;
-					for (let side in m.faces) {
-						if(m.faces[side].getTexture()==false){
-							del = true;
-						}
-					}
-					if(del){
-						m.remove();
-					}
-					
-				})
-			}
-		}
-		flash_action = new Action({
-			id: 'remove_cube_missing_texture',
-			name: 'Remove Cube Missing Texture',
-			category: 'textures',
-			icon: 'flash_on',
-			click() {
-				mt_highlighter.start();
-			}
-		});
-		MenuBar.addAction(flash_action, 'filter')
-	},
-	onunload() {
-		flash_action.delete();
-	}
-});
-
-})()
+(function () {
+  var flash_action;
+  Plugin.register("remove_cube_missing_texture", {
+    title: "Remove Cube Missing Texture",
+    icon: "flash_on",
+    author: "kigkosa",
+    description: "Remove Cube Missing Texture",
+    version: "1.1.1",
+    variant: "both",
+    onload() {
+      const mt_highlighter = {
+        i: 0,
+        start: function () {
+          mt_highlighter.i = 0;
+          mt_highlighter.flash();
+        },
+        flash: function () {
+          Cube.all.forEach((m, i) => {
+            let del = false;
+            for (let side in m.faces) {
+              if (m.faces[side].getTexture() == false) {
+                del = true;
+              }
+            }
+            if (del) {
+              Undo.initEdit({ elements: [m] });
+              m.remove();
+              Undo.finishEdit("Remove cube");
+            }
+          });
+        },
+      };
+      flash_action = new Action({
+        id: "remove_cube_missing_texture",
+        name: "Remove Cube Missing Texture",
+        category: "textures",
+        icon: "flash_on",
+        click() {
+          mt_highlighter.start();
+        },
+      });
+      MenuBar.addAction(flash_action, "filter");
+    },
+    onunload() {
+      flash_action.delete();
+    },
+  });
+})();
